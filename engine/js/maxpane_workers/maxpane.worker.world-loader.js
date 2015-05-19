c2d048f7734e9140051a9286bb5ad4bffc617452
Lars van der Schans <larsvanderschans@gmail.com>
2015-05-20 00:57:55 +0200

    Start loading materials from son

--- engine/js/maxpane_workers/maxpane.worker.world-loader.js
+++ engine/js/maxpane_workers/maxpane.worker.world-loader.js
@@ -1,25 +1,49 @@
 // Webworker to load world data from JSON file
 
-var loadJSON = function (path, success, error) {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                if (success)
-                    success(JSON.parse(xhr.responseText));
-            } else {
-                if (error)
-                    error(xhr);
-            }
-        }
-    };
-    xhr.open("GET", path, true);
-    xhr.send();
-};
+function get(url) {
+    // Return a new promise.
+    return new Promise(function(resolve, reject) {
+        // Do the usual XHR stuff
+        var req = new XMLHttpRequest();
+        req.open('GET', url);
 
-onmessage = function(world) {
-    loadJSON(world.data.url,
-        function(data) { postMessage(data); },
-        function(xhr) { postMessage(xhr); }
-    );
+        req.onload = function() {
+            // This is called even on 404 etc
+            // so check the status
+            if (req.status == 200) {
+                // Resolve the promise with the response text
+                resolve(req.response);
+            }
+            else {
+                // Otherwise reject with the status text
+                // which will hopefully be a meaningful error
+                reject(Error(req.statusText));
+            }
+        };
+
+        // Handle network errors
+        req.onerror = function() {
+            reject(Error("Network Error"));
+        };
+
+        // Make the request
+        req.send();
+    });
+}
+
+onmessage = function(worldName) {
+    // Load world from worldName path
+    get(worldName.data.url)
+        .then(JSON.parse)
+        .then(function(response) {
+            var world = response;
+            // Load the materials and replace the url with object
+            get(world.materials).then(JSON.parse).then(function(materials){
+                world.materials = materials;
+                postMessage(response);
+            });
+        }, function(error) {
+        console.error("Failed!", error);
+        postMessage(error);
+    });
 };
